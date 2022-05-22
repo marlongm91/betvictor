100981c7b6c43395615f41d6f62a3169754dc54f
Marlon García <marlongarciam91@gmail.com>
2022-05-22 11:59:30 +0200

    first commit

---
+++ src/test/java/com/betvictor/challenge/BetvictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betvictor.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetvictorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
